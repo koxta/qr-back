7311f5b0097f0092a81847abe15ca85cbcc71d4c
giorgi maziashvili <gmaziashvili@live.com>
2019-06-06 14:17:32 -0700

    Fixed a typo

--- app/model/db.js
+++ app/model/db.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'mydb'
+    database : 'qr'
 });
 
 connection.connect(function(err) {

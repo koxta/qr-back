c8b1f03c48474fe7749d354835fca5c70cb2dc63
giorgi maziashvili <gmaziashvili@live.com>
2019-06-06 13:59:13 -0700

    Added mysql in server

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
-var express = require('express'),
+const express = require('express'),
 app = express(),
 port = process.env.PORT || 3000;
+
+const mysql = require('mysql');
+
+const mc = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"qr"
+});
+
+mc.connect();
+
+
+
+app.listen(port);
+
+
+
+mc.query("Select * from Products", function (err, res) {
+
+    if(err) {
+        console.log("error: ", err);
+    }
+    else{
+      console.log('tasks : ', res);  
+    }
+});   
+
+
